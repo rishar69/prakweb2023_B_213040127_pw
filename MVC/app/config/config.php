2a871de24cc7098923c5b7c247c5a198232998cc
rishar69 <harisharyarevano@gmail.com>
2023-10-23 14:42:02 +0700

    MVC -8

---
+++ MVC/app/config/config.php
@@ -0,0 +1,9 @@
+<?php  
+
+define('BASEURL', 'http://prakweb2023_b_213040127_pw.test/MVC/');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'phpmvc');
